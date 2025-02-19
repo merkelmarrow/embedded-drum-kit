c49df7cff94a925bd2637f891c9dade698bf42ec
merkelmarrow <69924036+merkelmarrow@users.noreply.github.com>
2025-02-19 12:48:56 +0000

    init: initialise pico 2W build environment

---
+++ drum-kit/drum-kit.cpp
@@ -0,0 +1,12 @@
+﻿// drum-kit.cpp : Defines the entry point for the application.
+//
+
+#include "drum-kit.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
