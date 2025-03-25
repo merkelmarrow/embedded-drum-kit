c6e2133763579f5ed32b56ec3aefb4d3bac8f5d1
merkelmarrow <marco.a.blackwell@gmail.com>
2025-03-25 17:53:50 +0000

    feat: set up double buffer DMA setup in AudioEngine constructor

--- src/audio.cpp
+++ src/audio.cpp
@@ -5,11 +5,15 @@
 #include "src/configs.hpp"
 #include "src/snare.hpp"
 #include <cstdint>
+#include <hardware/dma.h>
 #include <hardware/gpio.h>
+#include <hardware/irq.h>
 #include <hardware/spi.h>
 #include <hardware/structs/io_bank0.h>
 
-AudioEngine::AudioEngine() {
+AudioEngine::AudioEngine()
+    : dma_channel_(dma_claim_unused_channel(true)),
+      chain_dma_channel_(dma_claim_unused_channel(true)) {
   FUNCTION_PRINT("AudioEngine::AudioEngine\n");
 
   // initialise sample data
@@ -29,7 +33,86 @@ AudioEngine::AudioEngine() {
   // SPI_MSB_FIRST: most sig bit transmitted first
   spi_set_format(spi_, 16, SPI_CPOL_0, SPI_CPHA_0, SPI_MSB_FIRST);
 
+  // set the DAC pins to SPI
   gpio_set_function(DAC_SCK_PIN, GPIO_FUNC_SPI);
   gpio_set_function(DAC_MOSI_PIN, GPIO_FUNC_SPI);
   gpio_set_function(DAC_CS_PIN, GPIO_FUNC_SPI);
+
+  // MAIN DMA CONFIG
+
+  // creates a default configuration for our dma channel
+  // dma_channel_ holds the channel number we already claimed
+  dma_channel_config dma_config = dma_channel_get_default_config(dma_channel_);
+
+  // configure the main DMA channel to transfer 16-bit data
+  // (the dac requires 16 bits to be sent at a time)
+  channel_config_set_transfer_data_size(&dma_config, DMA_SIZE_16);
+
+  // set up a "data request" trigger from the SPI hardward
+  // this makes the DMA wait for the SPI TX FIFO to have room before
+  // transferring each piece of data, ensuring synchronisation.
+  // the "true" parameter means we're requesting for TX not RX
+  channel_config_set_dreq(&dma_config, spi_get_dreq(spi_, true));
+
+  // configure the DMA to incremement its read address after each transfer
+  channel_config_set_read_increment(&dma_config, true);
+
+  // configure the DMA not to incremement its write address.
+  // we always want to write to the same SPI data register location
+  // so each sample goes to the same hardware destination
+  channel_config_set_write_increment(&dma_config, false);
+
+  // CHAIN DMA CONFIG
+  // the chain DMA is a second DMA channel that automatically reconfigures
+  // the mian DMA channel when it finished the buffer
+
+  // get a default config for our chain DMA channel
+  dma_channel_config chain_config =
+      dma_channel_get_default_config(chain_dma_channel_);
+
+  // set the chain DMA to transfer 4 bytes (32 bits)
+  // because we're sending a memory address and the pico is a 32 bit system
+  channel_config_set_transfer_data_size(&chain_config, DMA_SIZE_32);
+
+  // don't increment the read address (we're reading from a fixed address)
+  // either bufferA's or bufferB's address
+  channel_config_set_read_increment(&chain_config, false);
+
+  // don't increment write address
+  channel_config_set_write_increment(&chain_config, false);
+
+  // this is what creates the double-buffer cyclical system
+  // when the chain DMA finishes, it will automatically restart the main DMA
+  // channel this creates a continuous cycle, main DMA -> chain DMA -> main DMA
+  channel_config_set_chain_to(&chain_config, dma_channel_);
+
+  // this function configures the detail of the DMA
+  dma_channel_configure(
+      // which dma channel to configure
+      chain_dma_channel_,
+
+      // address of configuration we just set up
+      &chain_config,
+
+      // destination: this is the address of where the dma will write to
+      // this is the control register of the main DMA channel that holds
+      // the "read" address". By writing here, we change where the main DMA
+      // reads from next. The "al3_read_addr_trig" means:
+      // al3: Alias 3 (a special address that also triggers the DMA)
+      // read_addr: the read address of the register
+      // trig: writing here triggers the dma to start
+      &dma_hw->ch[dma_channel_].al3_read_addr_trig,
+
+      // source: where the chain DMA will read from
+      // we set this to null for now but it will update dynamically
+      // in the irq handler to point to buffer A or B
+      nullptr,
+
+      // count: the number of transfers to do
+      // we only need to transfer once, the address of the next audio buffer
+      1,
+
+      // don't start the dma chain yet
+      // it will be triggered by the IRQ handler when the main DMA completes
+      false);
 }
